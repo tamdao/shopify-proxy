922c22af3f8bc051bf1d78d6a078a636281b0e66
Tam Dao <>
2022-11-10 09:33:07 +0700

    Implement shopify proxy

---
+++ index.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+const http = require('http')
+const https = require('https')
+const Koa = require('koa')
+const cors = require('@koa/cors')
+const shopifyProxy = require('./shopify-proxy')
+
+const app = new Koa()
+
+app.use(cors())
+app.use(shopifyProxy({ version: '2022-10' }))
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server is listening on ${process.env.PORT || 3000} port`)
+})
---
+++ shopify-proxy.js
@@ -0,0 +1,31 @@
+const proxy = require('koa-better-http-proxy')
+
+const PROXY_BASE_PATH = '/graphql'
+
+module.exports = function shopifyProxy(proxyOptions) {
+  return async function shopifyProxyMiddleware(ctx, next) {
+    if (ctx.path !== PROXY_BASE_PATH || ctx.method !== 'POST') {
+      await next()
+      return
+    }
+    // console.log('ctx.headers', ctx.headers)
+    await proxy(process.env.SHOP_URL, {
+      https: true,
+      parseReqBody: false,
+      headers: {
+        'Content-Type': 'application/graphql',
+        'X-Shopify-Storefront-Access-Token':
+          process.env.STOREFRONT_ACCESS_TOKEN,
+      },
+      proxyReqOptDecorator(proxyReqOpts) {
+        delete proxyReqOpts.headers['content-type']
+        return proxyReqOpts
+      },
+      proxyReqPathResolver() {
+        return `/api/${proxyOptions.version}/graphql.json`
+      },
+    })(ctx, noop)
+  }
+}
+
+async function noop() {}
